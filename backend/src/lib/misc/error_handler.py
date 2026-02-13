6c6cf24fc8cd3adbc4663e08abdf24d815625f7e
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-02-12 23:02:55 -0700

    Fixed the routines system. Going to work on it more tomorrow.

--- backend/src/lib/misc/error_handler.py
+++ backend/src/lib/misc/error_handler.py
@@ -73,7 +73,7 @@ class ErrorMessage:
     WORKOUT_NOT_FOUND = "Workout not found"
     SETTINGS_NOT_FOUND = "Settings not found"
     RESOURCE_NOT_OWNED = "Resource not found or not owned by user"
-    ENTRY_WITH_NAME_ALREADY_EXISTS = "An entry with the name '{name}' already exists"
+    ENTRY_WITH_NAME_ALREADY_EXISTS = "An entry with the name '{name}' already exists on the same date"
     
     # Operation errors
     FAILED_TO_CREATE = "Failed to create resource"
